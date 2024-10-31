a089d4c422b30802f778c145ec4bcd61877caaf0
Gray <sergey.petrenko@gmail.com>
2024-11-01 00:33:57 +0200

    update: astro config, package.json, wrangler.toml Optimize build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,27 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import Compress from "astro-compress";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(),
+    Compress({
+      img: true,
+      css: true,
+      js: true,
+      html: false,
+    })
+  ],
   postsPerPage: 15,
+  output: 'static',
+  
+  build: {
+    inlineStylesheets: 'auto',
+    splitting: true,
+    moduleCache: true
+  }
 });
