f42eab81d2965ff2ef41228e7556e707ea14bf0f
Gray <sergey.petrenko@gmail.com>
2024-11-03 09:06:50 +0200

    Update site URL in Astro configuration
    Changed the site URL in the Astro configuration file to "https://tg.blognot.co" for correct deployment destination. Removed an unused Node integration to streamline configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://tg.blognot.co",
   integrations: [
     mdx(), 
     sitemap(), 
